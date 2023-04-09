d663808eea920ad779bd686ccdc14035465c3e9a
philipostr <philipostroscki@yahoo.ca>
2023-04-09 15:25:10 -0400

    Initial page creation

---
+++ customer.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pethotel</title>
+</head>
+<body>
+    
+</body>
+</html>
