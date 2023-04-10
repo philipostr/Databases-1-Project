f303db08942da50aebccd73c03901a462e5cd10a
sabina1717 <ssolo008@uottawa.ca>
2023-04-09 22:27:47 -0400

    start employee.php

--- db.php
+++ db.php
@@ -14,6 +14,33 @@ function checkLogin($type, $un, $ps) {
            ." WHERE username = $1 AND password = $2";
     pg_prepare($GLOBALS['dbconn'], 'logincheck', $query);
     $result = pg_execute($GLOBALS['dbconn'], 'logincheck', [$un, $ps]);
-
+    echo $result;
     return pg_num_rows($result) > 0;
+}
+
+function getEmployeeSinAndName($username){
+    $query = "SELECT employee_sin, name  FROM employee WHERE username = $1 ";
+    // $query2 = "SELECT username  FROM employee WHERE username =". $username;
+    pg_prepare($GLOBALS['dbconn'], 'getEmployeeSinAndName', $query);
+    // $result = pg_query($GLOBALS['dbconn'], $query2);
+    $result = pg_execute($GLOBALS['dbconn'], 'getEmployeeSinAndName', [$username]);
+    
+    // echo print_r($result);
+    // if (!$result) {
+    //     echo "An error occured.\n";
+    //     exit;
+    // }
+    // while ($row = pg_fetch_row($result)) {
+    //     print_r($row);
+    // }
+    return (pg_fetch_row($result));
+}
+
+function getEmployeeHotel($sin){
+    $query = "SELECT * FROM works WHERE works.employee_sin = $1";
+    //  and works.hotel_name= hotel.hotel_name;";
+    pg_prepare($GLOBALS['dbconn'], 'getEmployeeHotel', $query);
+    $result = pg_execute($GLOBALS['dbconn'], 'getEmployeeHotel', [$sin]);
+    
+    return (pg_fetch_row($result));
 }
--- employee.php
+++ employee.php
@@ -1,5 +1,27 @@
 <?php
     session_start();
+    require_once 'db.php';
+    DBconnect();
+
+
+
+
+
+    $user = $_SESSION['username'];
+    
+    // print_r( getEmployeeInfo($user));
+    $empinf = ( getEmployeeSinAndName($user) );
+    // var_dump($empinf);
+    $sin = $empinf [0];
+    $name = $empinf [1];
+    $r = ( getEmployeeHotel($sin) );
+    $hotel = $r[1];
+    $position= $r[2];
+    // echo "<br/> <br/>";
+    var_dump($r);
+    // var_dump( getEmployeeHotel($sin));
+
+
 ?>
 <html>
 <head>
@@ -8,6 +30,20 @@
     <title>Alliance Hotels</title>
 </head>
 <body>
-    
+    <h1> <?php echo $hotel ?>  </h1>
+    <h2>Welcome, <?php echo $name ." ". $position ?> </h2>
+
+    <!-- <button>Covert booking to renting</button>
+    <button>Rent without booking</button> -->
+    <form method="POST" action="">
+        <label>Customer SIN: 
+            <input name="customersin" type="text" placeholder="sin" required>
+        </label><br>
+        
+        <input name="login" type="submit" value="Covert booking to renting">
+        <br>
+        <input name="login" type="submit" value="Rent without booking">
+        <p><?php echo isset($problem) ? $problem : '' ?></p>
+    </form>
 </body>
 </html>
